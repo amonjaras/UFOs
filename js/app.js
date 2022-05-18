1a36271920712a366d235862d93d99e5dccb5ae3
amonjaras <100452376+amonjaras@users.noreply.github.com>
2022-05-18 12:01:17 -0400

    app completed

--- js/app.js
+++ js/app.js
@@ -19,4 +19,29 @@ data.forEach((dataRow) => {
         }
     );    
 });
+
+// Add filters for date data
+function handleClick() {
+    // Grab the datetime value form the filter
+    let date = d3.select("#datetime").property("value");
+    let filteredData = tableData;
+
+    // Using if statement to filter only entered date
+    if (date){
+        filteredData = filteredData.filter(row => row.datetime === date);
+    };
+
+    // Rebuild the table using the filtered data
+    // @NOTE: If no date was entered, then filteredData will
+    // just be the original tableData.
+    buildTable(filteredData);
+}
+
+// Atach an event to listen for the form button
+d3.selectAll("#filter-btn").on("click", handleClick);
+
+// Build the table when the page loads
+buildTable(tableData);
+
+
 }
